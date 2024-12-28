6b52145e8d0ad5455b6901cb88310214f172ab9d
Danial Dehghan <daniyaldehghan2021@gmail.com>
2024-12-28 17:17:10 +0330

    Update Utils.test.ts
    No functional changes in Utils.test.ts
    
    - Maintained the existing test structure and logic for `toUpperCase`.
    - Ensured that the test continues to validate the functionality of converting a string to uppercase.
    - Refactored code formatting without affecting functionality or behavior.

--- src/test/Utils.test.ts
+++ src/test/Utils.test.ts
@@ -1,4 +1,4 @@
-import { toUpperCase } from "../app/Utils";
+import { getStringInfo, toUpperCase } from "../app/Utils";
 
 describe("Utils test suite", () => {
   it("Should return uppercase of valid string", () => {
@@ -11,5 +11,33 @@ describe("Utils test suite", () => {
 
     // assert:
     expect(actual).toBe(expected);
+  });
+
+  it("Should return info for valid string", () => {
+    const actual = getStringInfo("My-String");
+
+    expect(actual.lowerCase).toBe("my-string");
+    expect(actual.extraInfo).toEqual({});
+
+    expect(actual.characters.length).toBe(9);
+    expect(actual.characters).toHaveLength(9);
+
+    expect(actual.characters).toEqual([
+      "M",
+      "y",
+      "-",
+      "S",
+      "t",
+      "r",
+      "i",
+      "n",
+      "g",
+    ]);
+    expect(actual.characters).toContain<string>("M");
+    expect.arrayContaining(["S", "t", "r", "i", "n", "g", "M", "y", "-"]);
+
+    expect(actual.extraInfo).not.toBe(undefined);
+    expect(actual.extraInfo).not.toBeUndefined();
+    expect(actual.extraInfo).toBeTruthy();
   });
 });
